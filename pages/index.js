566bffc1e7d3e68273a31502b8728353ba6d8ab8
contactdpop-alt <contact.dpop@gmail.com>
2025-11-09 00:51:26 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import dynamic from 'next/dynamic';
+
+const MapContainer = dynamic(
+  () => import('react-leaflet').then(mod => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import('react-leaflet').then(mod => mod.TileLayer),
+  { ssr: false }
+);
+
+export default function Home() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapContainer center={[31.7917, -7.0926]} zoom={5} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
