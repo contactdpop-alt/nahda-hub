bda4de1898f326f557623353ca8695b3bbff6414
contactdpop-alt <contact.dpop@gmail.com>
2025-11-09 01:26:43 -0500

    Create apps.js

---
+++ pages/apps.js
@@ -0,0 +1,8 @@
+export default function Apps() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Apps</h1>
+      <p>This section will host additional applications and tools related to election data or civic engagement. Check back later for new features.</p>
+    </div>
+  );
+}
