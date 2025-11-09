2b2e27f3d103ecd145aa9f252964aaba1b315309
contactdpop-alt <contact.dpop@gmail.com>
2025-11-09 03:27:02 -0500

    Add elections page

---
+++ pages/elections/index.jsx
@@ -0,0 +1,8 @@
+export default function ElectionsPage() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Elections 2021 Map</h1>
+      <p>This page will host the interactive elections 2021 map with turnout and party results.</p>
+    </div>
+  );
+}
